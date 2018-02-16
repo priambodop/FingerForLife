bce69ed315cae22b3749708e44c9736543106b76
priambodop <priambodop>
2018-02-16 17:39:20 +0700

    modif app.js again

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.use(function(req, res, next){
+  console.log("Start");
+  next();
+});
+
+app.get('/', function(request, response,next) {
   //response.render('routes/index')
   response.send('OOO WEEEE !!!');
+  next();
 });
+
+app.use('/',function(req,res){
+  console.log('End');
+});
+
+app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
