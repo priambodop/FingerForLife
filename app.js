0977357d7a6bd2745c212a21e2f4f9e1406f5674
priambodop <priambodop>
2018-02-18 22:32:43 +0700

    add home page

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var app = express();
+const path = require('path');
 
 //app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+
 app.get('/', function(request, response) {
   //response.render('routes/index')
-  response.send('SO THIS IS A GIANT STEP FOR HUMANITY ! ');
-  //response.render('routes/index');
+  //response.send('SO THIS IS A GIANT STEP FOR HUMANITY ! ');
+  response.render('home');
 });
 
 app.set('port', (process.env.PORT || 5000));
