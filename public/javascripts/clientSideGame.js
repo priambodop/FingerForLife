8c384dac166624e78deec63b798cda8e4231a660
priambodop <priambodop>
2018-03-05 22:56:35 +0700

    separating javascript file

---
+++ public/javascripts/clientSideGame.js
@@ -0,0 +1,7 @@
+function getRandInt(){
+  var text = "";
+  text = Math.floor(Math.random() * (999999 - 111111)) + 111111;
+  document.getElementById("code").innerHTML = text;
+}
+
+window.onload = getRandInt;
