dbfb2aa98336a40a042bfa318ecfef3b15273ac3
priambodop <priambodop>
2018-03-05 22:41:30 +0700

    cleaning out unnecessary file

--- app.js
+++ app.js
@@ -5,11 +5,6 @@ const path = require('path');
 
 var http = require('http').Server(app);
 
-var socketIO = require('socket.io');
-var io = socketIO(http);
-
-var finger = require('./public/javascripts/clientSideGame');
-
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -23,11 +18,8 @@ app.get('/sync', function(req, res){
   res.render('sync');
 });
 
-io.on('connection', function(socket){
-  finger.initGame(io, socket);
-});
-
 app.set('port', (process.env.PORT || 5000));
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
