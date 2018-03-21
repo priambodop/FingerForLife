84dfcaffc84866fd6c0450d3ef4d2f36671e48b3
priambodop <priambodop>
2018-03-21 10:42:08 +0700

    modified several files and added socket.io

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path = require('path');
 
 var http = require('http').Server(app);
 
+var socketIO = require('socket.io');
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -19,6 +22,12 @@ app.get('/sync', function(req, res){
 });
 
 app.set('port', (process.env.PORT || 5000));
+
+var io = socketIO(http);
+
+io.on('connection',function(socket){
+  console.log('Client Connected...');
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
---
+++ public/javascripts/serverSideGame.js
@@ -0,0 +1,13 @@
+// const express = require('express');
+// const app = express();
+//
+// const http = require('http').Server(app);
+//
+// const socketIO = require('socket.io');
+// const io = socketIO(http);
+//
+// io.on('connection',function(socket){
+//   socket.on('startClick', function(msg){
+//     io.emit('clicked', 'msg');
+//   });
+// });
