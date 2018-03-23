71c2800226acea035338f01ece4c328f48b3c259
priambodop <priambodop>
2018-03-23 21:30:11 +0700

    REFINE the overall server connection

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
+var createError = require('http-errors');
 var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var homeRouter = require('./routes/homeRoutes');
+var syncRouter = require('./routes/syncRoutes');
+
 var app = express();
 
-const path = require('path');
-
-var http = require('http').Server(app);
-
-var socketIO = require('socket.io');
-
-
-app.use(express.static(path.join(__dirname, 'public')));
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response, next) {
-  response.render('home');
-  next();
+app.use(logger('dev'));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', homeRouter);
+app.use('/sync', syncRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.get('/sync', function(req, res){
-  res.render('sync');
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.set('port', (process.env.PORT || 5000));
-
-var io = socketIO(http);
-
-io.on('connection',function(socket){
-  console.log('Client Connected...');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+module.exports = app;
