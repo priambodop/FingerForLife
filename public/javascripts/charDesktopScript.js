e9dd1ebfd548b757925cc73f52432c2c3e87d922
priambodop <priambodop>
2018-04-09 07:33:22 +0700

    Styling every page and finishing everything until choose a char

--- public/javascripts/charDesktopScript.js
+++ public/javascripts/charDesktopScript.js
@@ -12,12 +12,52 @@ socket.on('charDesktopAcc', function(msg){
 //The choosen character is shown.
 socket.on('charSelecting', function(msg){
   if (msg.id === players[0].id) {
-    console.log(`Player 1 is selecting char number ${msg.val}`);
-    var player1 = document.getElementById("player1Char");
-    player1.innerHTML = `Your char is number ${msg.val}`;
+    var imagep1 = document.getElementById("imagePlayer1");
+    if (msg.val == 1) {
+      imagep1.innerHTML = '<img class="imgChar" src="images/BrocoDude.png" alt="finger">';
+    }else if (msg.val == 2) {
+      imagep1.innerHTML = '<img class="imgChar" src="images/DabuDonut.png" alt="finger">';
+    }else if (msg.val == 3) {
+      imagep1.innerHTML = '<img class="imgChar" src="images/GrapeYoda.png" alt="finger">';
+    }else{
+      imagep1.innerHTML = '<img class="imgChar" src="images/SummerEgg.png" alt="finger">';
+    }
   }else{
-    console.log(`Player 2 is selecting char number ${msg.val}`);
-    var player2 = document.getElementById("player2Char");
-    player2.innerHTML = `Your char is number${msg.val}`;
+    var imagep2 = document.getElementById("imagePlayer2");
+    if (msg.val == 1) {
+      imagep2.innerHTML = '<img class="imgChar" src="images/BrocoDude.png" alt="finger">';
+    }else if (msg.val == 2) {
+      imagep2.innerHTML = '<img class="imgChar" src="images/DabuDonut.png" alt="finger">';
+    }else if (msg.val == 3) {
+      imagep2.innerHTML = '<img class="imgChar" src="images/GrapeYoda.png" alt="finger">';
+    }else{
+      imagep2.innerHTML = '<img class="imgChar" src="images/SummerEgg.png" alt="finger">';
+    }
+  }
+});
+
+socket.on('charSent', function(msg){
+  if (msg.id === players[0].id) {
+    var play1 = document.getElementById("player1Char");
+    if (msg.val == 1) {
+      play1.innerHTML = 'You choose Broco Dude';
+    }else if (msg.val == 2) {
+      play1.innerHTML = 'You choose Dabu Donut';
+    }else if (msg.val == 3) {
+      play1.innerHTML = 'You choose Grape Yoda';
+    }else{
+      play1.innerHTML = 'You choose Summer Egg';
+    }
+  }else {
+    var play2 = document.getElementById("player2Char");
+    if (msg.val == 1) {
+      play2.innerHTML = 'You choose Broco Dude';
+    }else if (msg.val == 2) {
+      play2.innerHTML = 'You choose Dabu Donut';
+    }else if (msg.val == 3) {
+      play2.innerHTML = 'You choose Grape Yoda';
+    }else{
+      play2.innerHTML = 'You choose Summer Egg';
+    }
   }
 });
